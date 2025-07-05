cbf0cc9e1c6cfb143939bab8f23f4743cee391a3
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-05 18:54:36 +0530

    Create count_digits.cpp

---
+++ maths/count_digits.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main() {
+  int n;
+  cin>>n;
+  int x=log10(n)+1;
+  cout<<x;
+    return 0;
+}
+/*
+output
+638
+3
+*/
