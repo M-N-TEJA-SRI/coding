ef634e8d30aa1e05c47f9799b3793433ddbb3442
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-06 07:58:20 +0530

    Create count_inversions.cpp

---
+++ array/hard/count_inversions.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+  int n,i,j,c=0;
+  cin>>n;
+  int a[n];
+  for(i=0;i<n;i++)
+ {
+     cin>>a[i];
+ }
+ for(i=0;i<n;i++){
+     for(j=i+1;j<n;j++){
+         if(a[i]>a[j])
+         c++;
+     }
+ }
+ cout<<c;
+    return 0;
+}
+/*
+output
+5
+9 0 7 4 1
+7
+*/
