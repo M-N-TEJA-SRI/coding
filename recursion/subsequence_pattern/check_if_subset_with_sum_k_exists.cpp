51ed14693d8115bb08a0f30dc05558cc4ff26cf3
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-07 10:00:51 +0530

    Create check_if_subset_with_sum_k_exists.cpp

---
+++ recursion/subsequence_pattern/check_if_subset_with_sum_k_exists.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+bool subset(int a[],int n,int i,int sum,int k){
+    if(i>=n){
+        return sum==k;
+    }
+    return subset(a,n,i+1,sum+a[i],k)||subset(a,n,i+1,sum,k);
+}
+int main() {
+int n,k,i,sum=0;
+cin>>n>>k;
+int a[n];
+for(i=0;i<n;i++){
+    cin>>a[i];
+}
+i=0;
+cout<<subset(a,n,i,sum,k);
+    return 0;
+}
+/*
+output
+3 7
+1 2 3
+0
+  */
